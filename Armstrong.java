38a961f0fcc122798f58cc46c9102160f24e37d7
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2026-01-03 21:16:29 +0530

    Armstrong concept

---
+++ Armstrong.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Armstrong{
+    public static void main(String[] args){
+        int n=153;
+        int t=n;
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+            sum=sum+d*d*d;
+            n=n/10;
+        }
+        System.out.println((t == sum)?"armstrong ":"Not armstromng");
+        
+    }
+}
