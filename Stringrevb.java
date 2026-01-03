3bc2f88bf38f9531a539d9ab88fa657428ce6687
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2026-01-03 08:15:31 +0530

    Create Stringrevb.java

---
+++ Stringrevb.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Stringrevb
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		StringBuilder sb=new StringBuilder(str);
+		sb.reverse();
+		System.out.println(sb.toString());
+	}
+}
