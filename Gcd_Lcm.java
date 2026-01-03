470137f3b74538bceed4454bab77b7a2c098d67c
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2026-01-03 21:33:49 +0530

    Add GCD and LCM calculation in Gcd_Lcm.java

---
+++ Gcd_Lcm.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Gcd_Lcm{
+        public static int gcd(int a,int b){
+            if(b==0){
+                return a;
+            }
+            return gcd(b,a%b);
+            
+        }
+        public static int lcm(int a,int b){
+            return a*b/gcd(a,b);
+        }
+        public static void main(String[] args){
+            int a=20,b=30;
+            System.out.println(gcd(a,b));
+            System.out.println(lcm(a,b));
+        }
+    
+}
