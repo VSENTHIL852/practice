f268a70d4505c29c4e75cac81a0e32711aa51d3a
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2026-01-03 08:44:19 +0530

    using bitwise
    Implement a program to swap two integers using bitwise XOR.

---
+++ Swapp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Swapp{
+	public static void main(String[] args) {
+        int a=10,b=20;
+	    a=a^b;
+	    b=a^b;
+	    a=a^b;
+	    System.out.println(a);
+	    System.out.println(b);
+	    
+	}
+}
