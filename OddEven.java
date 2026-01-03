45c50ebdff78c1c555ea3209bc822fc8bda85255
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2026-01-03 08:39:59 +0530

    normal mod using

--- OddEven.java
+++ OddEven.java
@@ -3,7 +3,7 @@ public class OddEven {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		if(n%2 != 0) {
+		if(n%2 == 0) {
 			System.out.println("Even");
 		}
 		else {
